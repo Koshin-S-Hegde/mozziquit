d064b9319df9e190842e2c9244d94b5f9148c2c2
Vivek Neer <139262048+VivekNeer@users.noreply.github.com>
2024-09-21 10:20:21 +0530

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ class App extends React.Component {
     render = () => {
         return (
             <>
+            <h1 className="text-9xl font-bold underline bg-red-800">
+             Hello world!
+            </h1>
             </>
         )
     }

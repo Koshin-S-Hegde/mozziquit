d929c73dba592a7bfdb382528de847b4b7a64c68
kosh <kosh@kosh-web.cfd>
2024-09-21 16:26:18 +0530

    Add 'Inspiring Change, Building Futures' and 3 cards to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,69 @@
-// import React from 'react';
+import React from "react";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home page.</p>
-    </div>
-  );
-};
+
+class Home extends React.Component {
+    render = () => {
+        return (
+            <div className="my-10">
+                <div className="text-2xl font-bold mb-10 text-center">
+                    Inspiring Change, Building Futures
+                </div>
+                <div className="flex justify-evenly text-justify flex-wrap flex-grow-1">
+                    <div className="min-w-4 max-w-[30rem]">
+                        <div className="border-dashed border-2 border-black px-10 w-fit py-3 rounded-3xl mt-5">
+                            <div className="font-semibold text-xl">
+                                Who we are?
+                            </div>
+                            <div className="">
+                                MozziQuit is an innovative startup dedicated to eradicating mosquito-
+                                borne diseases. We specialize in the design and manufacture of patented
+                                mosquito traps that effectively kill mosquitoes without the use of harmful
+                                chemicals or radiation.
+                            </div>
+                            <div className="mt-3 font-semibold text-xl">
+                                Who we are?
+                            </div>
+                            <div className="">
+                                MozziQuit is an innovative startup dedicated to eradicating mosquito-
+                                borne diseases. We specialize in the design and manufacture of patented
+                                mosquito traps that effectively kill mosquitoes without the use of harmful
+                                chemicals or radiation.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="min-w-4 max-w-[30rem] flex flex-col justify-evenly">
+                        <div className="border-dashed border-2 border-black px-10 w-fit py-3 rounded-3xl mt-5">
+                            <div className="font-semibold text-xl">
+                                Vision
+                            </div>
+                            <div className="">
+                                To be the global
+                                leader in mosquito
+                                control technology,
+                                ensuring a healthier
+                                future for all
+                                communities.
+                            </div>
+                        </div>
+                        <div className="border-dashed border-2 border-black px-10 w-fit py-3 rounded-3xl mt-5">
+                            <div className="mt-3 font-semibold text-xl">
+                                Mission
+                            </div>
+                            <div className="">
+                                To make the entire
+                                world free of the
+                                mosquito menace
+                                through innovative
+                                and sustainable
+                                solutions.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
+
 
 export default Home;
